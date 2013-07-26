4d3890946d5f8bb53bcf1b3c98e9ae9246abc0c1
Jakob Hohlfeld <jakob.hohlfeld@netronaut.de>
2013-07-26 20:15:49 +0200

    Adding yui docs

---
+++ doc/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "streamloader.Loader"
+    ],
+    "modules": [
+        "streamloader"
+    ],
+    "allModules": [
+        {
+            "displayName": "streamloader",
+            "name": "streamloader",
+            "description": "The streamloader object takes care of loading an html\ninto a stream (string object) and at the same time\ndistributes this stream as a number of sections.\n\nThe streamloader object returns a pormise\nwith an html string loaded via ajax.\n\nUse it's load() method to get started."
+        }
+    ]
+} };
+});
